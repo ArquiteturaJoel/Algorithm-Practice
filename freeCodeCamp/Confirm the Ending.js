7554aeb73fa549f709b8fb6a037bd5da6097a53e
Ming Yong <mingyong.dev@gmail.com>
2019-06-09 23:07:05 +0800

    Confirm the Ending

--- freeCodeCamp/Confirm the Ending.js
+++ freeCodeCamp/Confirm the Ending.js
@@ -18,6 +18,5 @@ function confirmEnding(str, target) {
 function confirmEnding(str, target) {
   // "Never give up and good luck will find you."
   // -- Falcor
-  
   return str.slice(str.length - target.length) === target;
 }
