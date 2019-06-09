122ff10d37463b6da18a9c6f3ee59b45c3503d19
Ming Yong <mingyong.dev@gmail.com>
2019-06-09 13:08:43 +0800

    Confirm the Ending

---
+++ freeCodeCamp/Confirm the Ending.js
@@ -0,0 +1,23 @@
+/*Confirm the Ending
+- confirmEnding("Open sesame", "same") should return true.
+- confirmEnding("Open sesame", "pen") should return false. 
+*/
+
+//my solution
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+  return (str.split("")
+        .splice(str.length-(target.length),str.length)
+        .join("") 
+        == target)
+}
+
+//fcc solution
+//intermediate
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+  
+  return str.slice(str.length - target.length) === target;
+}
